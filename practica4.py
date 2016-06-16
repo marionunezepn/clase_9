3f7046bc15315f4dede6e06c84fab6582e5b4387
Mario Nuñez <pepeseya@hotmail.com>
2016-06-16 08:26:33 -0500

    clase9
    ejercicios realizados en clase

---
+++ practica4.py
@@ -0,0 +1,8 @@
+import turtle
+t=turtle.Pen()
+x=(int(input("Numero de lados")))
+
+for x in range(1,x):
+	t.forward(50)
+	t.left(90)
+turtle.getscreen()._root.mainloop()
---
+++ practica5.py
@@ -0,0 +1,10 @@
+import turtle
+t=turtle.Pen()
+
+import turtle
+t=turtle.Pen()
+
+for x in range(0,5):
+	t.forward(200)
+	t.left(144)
+turtle.getscreen()._root.mainloop()
